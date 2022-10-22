8c739ee117ff3cb0c6ba4675a053ee0c9bac0242
Mano <manossef@gmail.com>
2022-10-22 08:29:59 +0100

    Rename authentication -> auth

--- {{cookiecutter.project_slug}}/mycoolproject/auth/apps.py
+++ {{cookiecutter.project_slug}}/mycoolproject/auth/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class AuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "mycoolproject.authentication"
+    name = "mycoolproject.auth"
+    label = "custom_auth"
