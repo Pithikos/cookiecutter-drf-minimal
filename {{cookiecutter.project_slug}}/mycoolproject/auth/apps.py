95b3a5d4e087857718595cdec46958ca88db4892
Mano <manossef@gmail.com>
2022-10-22 12:06:05 +0100

    Add User to admin

--- {{cookiecutter.project_slug}}/mycoolproject/auth/apps.py
+++ {{cookiecutter.project_slug}}/mycoolproject/auth/apps.py
@@ -5,3 +5,4 @@ class AuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mycoolproject.auth"
     label = "custom_auth"
+    verbose_name: str = "Authentication"
